581181154cf6d616201d178e37db676d98c08b3a
Eric020615 <ericjuncheng10@gmail.com>
2024-11-26 15:04:14 +0800

    fix[webcam-issue]: added error handling

--- src/components/dialog/VisitorCheckInDialog.tsx
+++ src/components/dialog/VisitorCheckInDialog.tsx
@@ -1,11 +1,5 @@
 import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react'
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-} from '@components/ui/dialog'
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@components/ui/dialog'
 import { IDetectedBarcode, Scanner } from '@yudiel/react-qr-scanner'
 import Webcam from 'react-webcam'
 import { getBase64FromImage } from '@lib/file'
@@ -13,6 +7,7 @@ import { useFaceAuth } from '@store/faceAuth/useFaceAuth'
 import { Button } from '@components/ui/button'
 import { Camera, Repeat, Upload } from 'lucide-react'
 import { useVisitor } from '@store/visitor/useVisitor'
+import { DialogDescription } from '@radix-ui/react-dialog'
 
 interface VisitorCheckInDialogProps {
     open: boolean
@@ -22,119 +17,149 @@ interface VisitorCheckInDialogProps {
 const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
     const [scanned, setScannedText] = useState('')
     const [isVerified, setIsVerified] = useState(false)
-    const [webcamError, setWebcamError] = useState<string | null>(null) // Webcam error state
-    const [scannerError, setScannerError] = useState<string | null>(null) // Scanner error state
-    const [isWebcamAvailable, setIsWebcamAvailable] = useState<boolean>(false) // Track webcam availability
-    const [isLoading, setIsLoading] = useState<boolean>(true) // Track if the webcam availability is loading
-    const webcamRef = useRef<Webcam>(null)
+    const [webcamError, setWebcamError] = useState<string | null>(null)
+    const [scannerError, setScannerError] = useState<string | null>(null)
+    const [isWebcamAvailable, setIsWebcamAvailable] = useState<boolean>(false)
+    const [isLoading, setIsLoading] = useState<boolean>(true)
     const [faceImage, setFaceImage] = useState<string>('')
+
+    const webcamRef = useRef<Webcam>(null)
 
     const { uploadUserFaceAuthAction } = useFaceAuth()
     const { visitorDetails, verifyVisitorTokenAction, isValid, resetVisitorDetails } =
         useVisitor()
 
     const capture = useCallback(() => {
-        if (!webcamRef.current) return
-        const imageSrc = webcamRef.current.getScreenshot()
-        if (imageSrc == null) return
-        setFaceImage(imageSrc)
+        if (webcamRef.current) {
+            const imageSrc = webcamRef.current.getScreenshot()
+            if (imageSrc) setFaceImage(imageSrc)
+        }
     }, [webcamRef])
 
-    const retake = () => {
-        setFaceImage('')
-    }
+    const retake = () => setFaceImage('')
 
     const uploadImage = async () => {
-        if (faceImage == null) return
-        let base64 = await getBase64FromImage(faceImage)
-        await uploadUserFaceAuthAction({
-            faceData: base64,
-        })
-        setOpen(false)
+        if (faceImage) {
+            const base64 = await getBase64FromImage(faceImage)
+            await uploadUserFaceAuthAction({ faceData: base64 })
+            setOpen(false)
+        }
+    }
+
+    const checkWebcamAvailability = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+            if (stream) setIsWebcamAvailable(true)
+        } catch (error: any) {
+            setWebcamError(error.message)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    const verifyVisitorToken = async () => {
+        if (scanned) {
+            await verifyVisitorTokenAction(scanned)
+            if (isValid) {
+                setIsVerified(true)
+                setScannerError(null)
+            }
+        }
+    }
+
+    const onScan = (detectedCodes: IDetectedBarcode[]) => {
+        const firstDetectedCode = detectedCodes[0]
+        if (firstDetectedCode?.rawValue) {
+            setScannedText(firstDetectedCode.rawValue)
+            setScannerError(null)
+        } else {
+            setScannerError('Invalid QR Code')
+        }
+    }
+
+    const handleError = (error: unknown, type: 'webcam' | 'scanner') => {
+        const errorMessage =
+            error instanceof Error ? error.message : 'An unknown error occurred'
+        if (type === 'webcam') setWebcamError(errorMessage)
+        if (type === 'scanner') setScannerError(errorMessage)
     }
 
     useEffect(() => {
-        if (!open) {
-            setScannedText('')
-            setIsVerified(false)
-            setFaceImage('')
-            setScannerError(null)
-            resetVisitorDetails()
+        if (open) checkWebcamAvailability()
+        return () => {
+            if (!open) {
+                setScannedText('')
+                setIsVerified(false)
+                setFaceImage('')
+                setScannerError(null)
+                resetVisitorDetails()
+            }
         }
     }, [open])
 
     useEffect(() => {
-        const verifyVisitorToken = async () => {
-            if (scanned) {
-                await verifyVisitorTokenAction(scanned)
-                if (isValid) {
-                    setIsVerified(true)
-                    setScannerError(null) // Clear scanner error after verification
-                }
-            }
-        }
-        verifyVisitorToken()
-    }, [scanned, isValid, verifyVisitorTokenAction])
-
-    const onScan = (detectedCodes: IDetectedBarcode[]) => {
-        if (detectedCodes && detectedCodes.length > 0) {
-            const firstDetectedCode = detectedCodes[0]
-            if (firstDetectedCode.rawValue) {
-                setScannedText(firstDetectedCode.rawValue)
-                setScannerError(null) // Reset scanner error if QR code is detected
-            } else {
-                setScannerError('Invalid QR Code') // Handle case if rawValue is not found
-            }
-        } else {
-            setScannerError('No QR Code detected') // Handle case where no QR code was detected
-        }
-    }
-
-    const onWebcamError = (error: unknown) => {
-        if (error instanceof Error) {
-            setWebcamError(error.message) // Store the error message instead of the object
-        } else if (error instanceof DOMException) {
-            setWebcamError(error.message) // Handle DOMException error messages properly
-        } else {
-            setWebcamError('An unknown error occurred')
-        }
-    }
-
-    const onScannerError = (error: unknown) => {
-        if (error instanceof Error) {
-            setScannerError(error.message) // Store the error message
-        } else if (error instanceof DOMException) {
-            setScannerError(error.message) // Handle DOMException errors
-        } else {
-            setScannerError('An unknown scanner error occurred')
-        }
-    }
-
-    // Check if the webcam is available when the component mounts
-    useEffect(() => {
-        const checkWebcamAvailability = async () => {
-            try {
-                const devices = await navigator.mediaDevices.enumerateDevices()
-                const videoDevices = devices.filter(
-                    (device) => device.kind === 'videoinput'
-                )
-                setIsWebcamAvailable(videoDevices.length > 0) // Set webcam availability based on the number of video devices
-                setIsLoading(false) // Stop loading once the webcam check is complete
-            } catch (error) {
-                setWebcamError('Error checking webcam availability')
-                setIsLoading(false) // Stop loading even if there's an error
-            }
-        }
-
-        checkWebcamAvailability()
-    }, [])
+        if (scanned) verifyVisitorToken()
+    }, [scanned, isValid])
 
     useEffect(() => {
-        if (isVerified) {
-            // Clear any existing webcam errors when verification succeeds
-            setWebcamError(null)
-        }
+        if (isVerified) setWebcamError(null)
     }, [isVerified])
+
+    const renderWebcam = () => {
+        if (webcamError) {
+            return (
+                <div className="flex items-center justify-center w-full h-96 border-2 border-red-500">
+                    <span className="text-red-500">{webcamError}</span>
+                </div>
+            )
+        }
+
+        if (isWebcamAvailable) {
+            return (
+                <Webcam
+                    height={600}
+                    width={600}
+                    ref={webcamRef}
+                    mirrored
+                    screenshotFormat="image/jpeg"
+                    screenshotQuality={0.3}
+                    onError={(error) => handleError(error, 'webcam')}
+                />
+            )
+        }
+
+        return (
+            <div className="flex items-center justify-center w-full h-96">
+                <span className="text-gray-500">Loading Webcam...</span>
+            </div>
+        )
+    }
+
+    const renderScanner = () => {
+        if (scannerError) {
+            return (
+                <div className="flex items-center justify-center w-full h-96 border-2 border-red-500">
+                    <span className="text-red-500">{scannerError}</span>
+                </div>
+            )
+        }
+
+        if (isLoading) {
+            return (
+                <div className="flex items-center justify-center w-full h-96">
+                    <span className="text-gray-500">Loading Webcam...</span>
+                </div>
+            )
+        }
+
+        return isWebcamAvailable ? (
+            <Scanner onScan={onScan} onError={(error) => handleError(error, 'scanner')} />
+        ) : (
+            <div className="text-center text-red-500">
+                No camera detected. Please connect a camera.
+            </div>
+        )
+    }
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -156,58 +181,23 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
                                 <div className="mt-2">
                                     {faceImage ? (
                                         <img src={faceImage} alt="webcam" />
-                                    ) : webcamError ? (
-                                        <div className="flex items-center justify-center w-full h-96 border-2 border-red-500">
-                                            <span className="text-red-500">
-                                                {webcamError}
-                                            </span>
-                                        </div>
-                                    ) : isWebcamAvailable ? (
-                                        <Webcam
-                                            height={600}
-                                            width={600}
-                                            ref={webcamRef}
-                                            mirrored={true}
-                                            screenshotFormat="image/jpeg"
-                                            screenshotQuality={0.3}
-                                            onError={onWebcamError} // Handle webcam error
-                                        />
                                     ) : (
-                                        <div className="flex items-center justify-center w-full h-96">
-                                            <span className="text-gray-500">
-                                                Loading Webcam...
-                                            </span>
-                                        </div>
+                                        renderWebcam()
                                     )}
                                 </div>
                                 <div className="flex justify-center mt-5">
                                     {faceImage ? (
                                         <div className="flex w-full justify-evenly">
-                                            <Button
-                                                type="button"
-                                                className="w-fit h-fit"
-                                                onClick={retake}
-                                            >
-                                                <Repeat className="mr-2 h-5 w-5" />
-                                                Retake
+                                            <Button onClick={retake}>
+                                                <Repeat className="mr-2 h-5 w-5" /> Retake
                                             </Button>
-                                            <Button
-                                                type="button"
-                                                className="w-fit h-fit"
-                                                onClick={uploadImage}
-                                            >
-                                                <Upload className="mr-2 h-5 w-5" />
-                                                Upload
+                                            <Button onClick={uploadImage}>
+                                                <Upload className="mr-2 h-5 w-5" /> Upload
                                             </Button>
                                         </div>
                                     ) : (
-                                        <Button
-                                            type="button"
-                                            className="w-fit h-fit"
-                                            onClick={capture}
-                                        >
-                                            <Camera className="mr-2 h-5 w-5" />
-                                            Capture
+                                        <Button onClick={capture}>
+                                            <Camera className="mr-2 h-5 w-5" /> Capture
                                         </Button>
                                     )}
                                 </div>
@@ -219,23 +209,7 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
                 ) : (
                     <div>
                         <div className="flex items-center justify-center mb-2">
-                            {scannerError ? (
-                                <div className="flex items-center justify-center w-full h-96 border-2 border-red-500">
-                                    <span className="text-red-500">{scannerError}</span>
-                                </div>
-                            ) : isLoading ? (
-                                <div className="flex items-center justify-center w-full h-96">
-                                    <span className="text-gray-500">
-                                        Loading Webcam...
-                                    </span>
-                                </div>
-                            ) : isWebcamAvailable ? (
-                                <Scanner onScan={onScan} onError={onScannerError} />
-                            ) : (
-                                <div className="text-center text-red-500">
-                                    No camera detected. Please connect a camera.
-                                </div>
-                            )}
+                            {renderScanner()}
                         </div>
                     </div>
                 )}
