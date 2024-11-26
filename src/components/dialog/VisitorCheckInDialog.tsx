294a210f797534158dc90557723a981059b70e8e
Eric020615 <ericjuncheng10@gmail.com>
2024-11-26 14:50:43 +0800

    fix[project]: fixing bugs

--- src/components/dialog/VisitorCheckInDialog.tsx
+++ src/components/dialog/VisitorCheckInDialog.tsx
@@ -21,9 +21,14 @@ interface VisitorCheckInDialogProps {
 
 const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
     const [scanned, setScannedText] = useState('')
-    const [isVerified, setIsVerified] = useState(false) // State to track if the token is verified
+    const [isVerified, setIsVerified] = useState(false)
+    const [webcamError, setWebcamError] = useState<string | null>(null) // Webcam error state
+    const [scannerError, setScannerError] = useState<string | null>(null) // Scanner error state
+    const [isWebcamAvailable, setIsWebcamAvailable] = useState<boolean>(false) // Track webcam availability
+    const [isLoading, setIsLoading] = useState<boolean>(true) // Track if the webcam availability is loading
     const webcamRef = useRef<Webcam>(null)
     const [faceImage, setFaceImage] = useState<string>('')
+
     const { uploadUserFaceAuthAction } = useFaceAuth()
     const { visitorDetails, verifyVisitorTokenAction, isValid, resetVisitorDetails } =
         useVisitor()
@@ -49,13 +54,11 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
     }
 
     useEffect(() => {
-        setFaceImage('')
-    }, [open == false])
-
-    // Reset scanned text when dialog is closed
-    useEffect(() => {
         if (!open) {
             setScannedText('')
+            setIsVerified(false)
+            setFaceImage('')
+            setScannerError(null)
             resetVisitorDetails()
         }
     }, [open])
@@ -65,25 +68,73 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
             if (scanned) {
                 await verifyVisitorTokenAction(scanned)
                 if (isValid) {
-                    setIsVerified(true) // Set as verified
+                    setIsVerified(true)
+                    setScannerError(null) // Clear scanner error after verification
                 }
             }
         }
         verifyVisitorToken()
-    }, [scanned])
+    }, [scanned, isValid, verifyVisitorTokenAction])
 
     const onScan = (detectedCodes: IDetectedBarcode[]) => {
         if (detectedCodes && detectedCodes.length > 0) {
             const firstDetectedCode = detectedCodes[0]
             if (firstDetectedCode.rawValue) {
-                setScannedText(firstDetectedCode.rawValue) // Store the rawValue of the scanned QR code
+                setScannedText(firstDetectedCode.rawValue)
+                setScannerError(null) // Reset scanner error if QR code is detected
             } else {
-                setScannedText('Invalid QR Code') // Handle case if rawValue is not found
+                setScannerError('Invalid QR Code') // Handle case if rawValue is not found
             }
         } else {
-            setScannedText('No QR Code detected') // Handle case where no QR code was detected
+            setScannerError('No QR Code detected') // Handle case where no QR code was detected
         }
     }
+
+    const onWebcamError = (error: unknown) => {
+        if (error instanceof Error) {
+            setWebcamError(error.message) // Store the error message instead of the object
+        } else if (error instanceof DOMException) {
+            setWebcamError(error.message) // Handle DOMException error messages properly
+        } else {
+            setWebcamError('An unknown error occurred')
+        }
+    }
+
+    const onScannerError = (error: unknown) => {
+        if (error instanceof Error) {
+            setScannerError(error.message) // Store the error message
+        } else if (error instanceof DOMException) {
+            setScannerError(error.message) // Handle DOMException errors
+        } else {
+            setScannerError('An unknown scanner error occurred')
+        }
+    }
+
+    // Check if the webcam is available when the component mounts
+    useEffect(() => {
+        const checkWebcamAvailability = async () => {
+            try {
+                const devices = await navigator.mediaDevices.enumerateDevices()
+                const videoDevices = devices.filter(
+                    (device) => device.kind === 'videoinput'
+                )
+                setIsWebcamAvailable(videoDevices.length > 0) // Set webcam availability based on the number of video devices
+                setIsLoading(false) // Stop loading once the webcam check is complete
+            } catch (error) {
+                setWebcamError('Error checking webcam availability')
+                setIsLoading(false) // Stop loading even if there's an error
+            }
+        }
+
+        checkWebcamAvailability()
+    }, [])
+
+    useEffect(() => {
+        if (isVerified) {
+            // Clear any existing webcam errors when verification succeeds
+            setWebcamError(null)
+        }
+    }, [isVerified])
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -105,7 +156,13 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
                                 <div className="mt-2">
                                     {faceImage ? (
                                         <img src={faceImage} alt="webcam" />
-                                    ) : (
+                                    ) : webcamError ? (
+                                        <div className="flex items-center justify-center w-full h-96 border-2 border-red-500">
+                                            <span className="text-red-500">
+                                                {webcamError}
+                                            </span>
+                                        </div>
+                                    ) : isWebcamAvailable ? (
                                         <Webcam
                                             height={600}
                                             width={600}
@@ -113,7 +170,14 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
                                             mirrored={true}
                                             screenshotFormat="image/jpeg"
                                             screenshotQuality={0.3}
+                                            onError={onWebcamError} // Handle webcam error
                                         />
+                                    ) : (
+                                        <div className="flex items-center justify-center w-full h-96">
+                                            <span className="text-gray-500">
+                                                Loading Webcam...
+                                            </span>
+                                        </div>
                                     )}
                                 </div>
                                 <div className="flex justify-center mt-5">
@@ -128,7 +192,7 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
                                                 Retake
                                             </Button>
                                             <Button
-                                                type="submit"
+                                                type="button"
                                                 className="w-fit h-fit"
                                                 onClick={uploadImage}
                                             >
@@ -155,7 +219,23 @@ const VisitorCheckInDialog = ({ open, setOpen }: VisitorCheckInDialogProps) => {
                 ) : (
                     <div>
                         <div className="flex items-center justify-center mb-2">
-                            <Scanner onScan={onScan} />
+                            {scannerError ? (
+                                <div className="flex items-center justify-center w-full h-96 border-2 border-red-500">
+                                    <span className="text-red-500">{scannerError}</span>
+                                </div>
+                            ) : isLoading ? (
+                                <div className="flex items-center justify-center w-full h-96">
+                                    <span className="text-gray-500">
+                                        Loading Webcam...
+                                    </span>
+                                </div>
+                            ) : isWebcamAvailable ? (
+                                <Scanner onScan={onScan} onError={onScannerError} />
+                            ) : (
+                                <div className="text-center text-red-500">
+                                    No camera detected. Please connect a camera.
+                                </div>
+                            )}
                         </div>
                     </div>
                 )}
--- src/screen/visitor/access-pass/index.tsx
+++ src/screen/visitor/access-pass/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import ActionConfirmationDialog from '@components/dialog/ActionConfirmationDialog'
 import { ITimeFormat } from '@config/constant'
 import { convertDateStringToFormattedString } from '@lib/time'
 import { useVisitor } from '@store/visitor/useVisitor'
@@ -21,36 +22,43 @@ const VisitorPassDetailsPage = () => {
     }, [token])
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
-            <h1 className="text-2xl font-bold text-gray-800 mb-6">Visitor Access Pass</h1>
+        <>
+            <ActionConfirmationDialog />
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
+                <h1 className="text-2xl font-bold text-gray-800 mb-6">
+                    Visitor Access Pass
+                </h1>
 
-            {/* Visitor Details */}
-            <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
-                <p className="text-lg font-medium text-gray-700 mb-2">
-                    <span className="font-bold">Name:</span>{' '}
-                    {visitorPassDetails.visitorName}
-                </p>
-                <p className="text-lg font-medium text-gray-700 mb-2">
-                    <span className="font-bold">Date:</span>{' '}
-                    {convertDateStringToFormattedString(
-                        visitorPassDetails.visitDateTime,
-                        ITimeFormat.date
-                    )}
-                </p>
-                <p className="text-lg font-medium text-gray-700">
-                    <span className="font-bold">Time:</span>{' '}
-                    {convertDateStringToFormattedString(
-                        visitorPassDetails.visitDateTime,
-                        ITimeFormat.time
-                    )}
-                </p>
-            </div>
+                {/* Visitor Details */}
+                <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+                    <p className="text-lg font-medium text-gray-700 mb-2">
+                        <span className="font-bold">Name:</span>{' '}
+                        {visitorPassDetails.visitorName}
+                    </p>
+                    <p className="text-lg font-medium text-gray-700 mb-2">
+                        <span className="font-bold">Date:</span>{' '}
+                        {convertDateStringToFormattedString(
+                            visitorPassDetails.visitDateTime,
+                            ITimeFormat.date
+                        )}
+                    </p>
+                    <p className="text-lg font-medium text-gray-700">
+                        <span className="font-bold">Time:</span>{' '}
+                        {convertDateStringToFormattedString(
+                            visitorPassDetails.visitDateTime,
+                            ITimeFormat.time
+                        )}
+                    </p>
+                </div>
 
-            <div className="mt-8 bg-white p-6 shadow-md rounded-lg">
-                <QRCode value={token as string} />
-                <p className="mt-4 text-sm text-gray-500">Scan this QR code for entry</p>
+                <div className="mt-8 bg-white p-6 shadow-md rounded-lg">
+                    <QRCode value={token as string} />
+                    <p className="mt-4 text-sm text-gray-500">
+                        Scan this QR code for entry
+                    </p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
